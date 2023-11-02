9c15811c2a3dbeac7b1d6b6d1db303459c379936
Powei Lin <poweilin1994@gmail.com>
2023-11-01 21:37:09 -0400

    instruction

--- instructions/parse.py
+++ instructions/parse.py
@@ -1,6 +1,9 @@
 from string import printable
 from dataclasses import dataclass
 
+SPACE_4 = '    '
+NEXT_LINE_INDENT = '\n'+SPACE_4
+
 @dataclass
 class Instruction:
     position: int
@@ -8,16 +11,19 @@ class Instruction:
     length_duration: str = None
     flags: str = None
     def __repr__(self) -> str:
-        return f"0x{self.position:02x}\n{self.command}\n{self.length_duration}\n{self.flags}\n"
+        return (f"def INS_0x{self.position:02X}():{NEXT_LINE_INDENT}"
+               f"# {self.command}{NEXT_LINE_INDENT}"
+               f"# {self.length_duration}{NEXT_LINE_INDENT}"
+               f"# {self.flags}{NEXT_LINE_INDENT}"
+               "pass\n")
 
 if __name__ == '__main__':
     
     current_file = "ins.tsv"
-    current_file = "pre.tsv"
+    # current_file = "pre.tsv"
 
     all_ins = []
     with open(current_file) as ifile:
-        current_i = '_'
         count = 0
         current_d = []
         for l in ifile.readlines()[1:]:
@@ -25,7 +31,6 @@ if __name__ == '__main__':
             li = new_string.split('\t')
             if li[0]:
                 count = 0
-                print(li[0])
                 current_d = []
                 s = int('0x'+li[0][0]+'0', base=16)
                 for i, ll in enumerate(li[1:]):
