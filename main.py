f5c29b1dba89a6602780ab589ea3bf86d4e26bf7
Powei Lin <poweilin1994@gmail.com>
2023-11-15 21:11:59 -0500

    small change

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from generated_opcode import CPU, Memory, INSTRUCTION_TABLE
 import cv2
 import numpy as np
 from debug import debug_ram
+from screen import LCD
 
 
 def fetch(cpu: CPU, memory: Memory) -> int:
@@ -13,36 +14,44 @@ def fetch(cpu: CPU, memory: Memory) -> int:
     return addr
 
 
-def tick(cpu: CPU, memory: Memory):
+def tick(cpu: CPU, memory: Memory, lcd: LCD):
     addr = fetch(cpu, memory)
     if addr == 0xcb:
         addr = fetch(cpu, memory)
         addr += 0x100
-    print(f"Fetched intruction 0x{addr:03x}")
+    # print(f"Fetched intruction 0x{addr:03x}")
     # execute
-    INSTRUCTION_TABLE[addr](cpu, memory)
+    cycle = INSTRUCTION_TABLE[addr](cpu, memory)
+    # print(f"current cycle: {cycle}")
+    return cycle
 
 
 if __name__ == '__main__':
     with open("DMG_ROM.bin", 'rb') as boot_rom:
         rom = boot_rom.read()
+    with open("Tetris.gb", 'rb') as cartridge:
+        game_rom = cartridge.read()
     cpu = CPU()
-    mem = Memory(rom, randomize=True)
+    mem = Memory(rom, game_rom, randomize=True)
+    lcd = LCD()
+    lcd.show()
 
     # arr = np.array(mem.ram, dtype=np.uint8).reshape(256, -1)
     # cv2.imshow("ram", arr)
     # cv2.waitKey(0)
+    count_cycle = 0
     try:
         count = 0
         while True:
-            print(f"{count}---------------")
-            print("CPU:")
-            print(cpu)
-            print("********")
-            tick(cpu, mem)
+            # print(f"{count}---------------")
+            # print("CPU:")
+            # print(cpu)
+            # print("********")
+            count_cycle += tick(cpu, mem, lcd)
             count += 1
     except NotImplementedError:
         ram_debug_img = debug_ram(mem.ram)
+        print(f"total cycle: {count_cycle}")
         cv2.imshow("ram", ram_debug_img)
         cv2.imwrite("ram.png", ram_debug_img)
         cv2.waitKey(0)
--- screen.py
+++ screen.py
@@ -1,3 +1,6 @@
+import numpy as np
+import cv2
+
 CONTROL_ADDR_RW = 0xff40
 STATUS_ADDR_RW = 0xff41
 SCROLL_Y_RW = 0xff42
@@ -7,7 +10,14 @@ LY_COMPARE_RW = 0xff45
 WINDOW_Y_POSITION_RW = 0xff4a
 WINDOW_X_POSITION_MINUS_7_RW = 0xff4b
 
+LCD_WIDTH = 160
+LCD_HEIGHT = 144
+
 
 class LCD:
     def __init__(self):
-        pass
+        self.screen = np.ones((LCD_HEIGHT, LCD_WIDTH), np.uint8) * 255
+
+    def show(self):
+        cv2.imshow("screen", self.screen)
+        cv2.waitKey(0)
