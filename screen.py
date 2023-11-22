945bc30b41a7f0e3e584fb83d35b67980913bb51
Powei Lin <poweilin1994@gmail.com>
2023-11-21 20:36:53 -0500

    tile

--- screen.py
+++ screen.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 from memory import Memory
 from enum import IntEnum
+from typing import List
 
 CONTROL_ADDR_RW = 0xff40
 STATUS_ADDR_RW = 0xff41
@@ -17,6 +18,25 @@ LCD_HEIGHT = 144
 BG_SIZE = 256
 
 GRAY_SHADES = [255, 170, 85, 0]
+
+
+def data_to_tile(data: List[int], palette: int = 0):
+    if len(data) != 16:
+        raise ValueError
+    i = np.array(data, dtype=np.uint8).reshape(8, 2)
+    i = np.unpackbits(i, axis=1)
+    i = (i[:, :8] + i[:, 8:] * 2)
+    p = [0b11 & (palette >> j * 2) for j in range(4)]
+    p = [GRAY_SHADES[j] for j in p]
+    i = np.vectorize(p.__getitem__)(i).astype(np.uint8)
+    print(i.dtype)
+
+    print(f"{palette:08b}")
+    print(i)
+    reszie = 10
+    i = np.repeat(np.repeat(i, reszie, axis=1), reszie, axis=0)
+    cv2.imshow("img", i)
+    cv2.waitKey(0)
 
 
 class PPUState(IntEnum):
@@ -74,6 +94,9 @@ class LCD:
     def tick(self, mem: Memory):
         self._check_control(mem.get(CONTROL_ADDR_RW))
         if self.lcd_display_enable > 0:
+            for i in range(0x8000, 0x9000, 16):
+                print(f"{i:04x}")
+                data_to_tile([mem.get(i + j) for j in range(16)], mem.get(0xff47))
 
             while self.ly < 144:
                 if self.mode_flag == 0:
@@ -90,6 +113,7 @@ class LCD:
                     pass
                 else:
                     raise ValueError
+                break
 
             self.ly = mem.get(Y_COORDINATE_R)
             self.lyc = mem.get(LY_COMPARE_RW)
